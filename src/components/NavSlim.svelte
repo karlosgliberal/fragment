9b8a64def7e5e276bca9e2bae019f1543004ec4d
Aitor Resano <aitor.rl@gmail.com>
2020-03-31 12:19:48 +0200

    NavSlim componente

---
+++ src/components/NavSlim.svelte
@@ -0,0 +1,20 @@
+<div class="flex bg-darkblue">
+    <div class="w-1/3 h-6 flex justify-star items-center">
+        <div class="pl-2">
+            <a href="">
+                <img src="back.svg" alt="" />
+            </a>
+
+        </div>
+    </div>
+    <div class="w-1/3 flex items-center justify-center h-6">
+        <div>
+            <img src="logo.svg" alt="Dance pose" />
+        </div>
+    </div>
+    <div class="w-1/3 flex items-center justify-end h-6">
+        <div class="pr-2">
+            <a class="uppercase Xfont-bold text-white" href="">About</a>
+        </div>
+    </div>
+</div>
--- src/routes/danceposeaitor/index.svelte
+++ src/routes/danceposeaitor/index.svelte
@@ -1,16 +1,15 @@
 <script>
     import Page from '../../components/Page.svelte';
+    import NavSlim from '../../components/NavSlim.svelte';
     import SimpleSketch from './sketch.svelte';
     import P5Canvas from '../../components/P5Canvas.svelte';
 </script>
 
+<NavSlim />
+
 <Page>
-    <div class="bg-darkblue text-white">
-        <h1 class="text-graysoft">AAAAAAAA</h1>
-        <p class="font-ptserif text-3xl font-bold">hola esto es una prueba</p>
-    </div>
     <h1 class="text-">Dancepose</h1>
     <p class="text">Pequeñas piezas de imagineria</p>
     <P5Canvas sketch={SimpleSketch} width="960" height="540" />
-    <a class="text-white" href="contacto">contacto</a>
+
 </Page>
