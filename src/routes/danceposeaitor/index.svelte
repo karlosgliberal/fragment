7059d12de9a27e48a8ebd565d39aa02a26423c80
Aitor Resano <aitor.rl@gmail.com>
2020-03-31 16:54:42 +0200

    Video

--- src/routes/danceposeaitor/index.svelte
+++ src/routes/danceposeaitor/index.svelte
@@ -11,20 +11,22 @@
 
     <NavSlim />
 
-    <div class="flex h-64">
+    <div class="flex pt-12 mt-2">
         <div class="w-4/12 bg-gray-300 h-auto flex items-center justify-center">
-            uno
+            <div class="w-full h-auto bg-white rounded-sm">
+                <p />
+            </div>
         </div>
-        <div
-            class="w-8/12 bg-gray-500 h-auto flex items-center justify-center
-            overflow-hidden">
-            aqui video
+        <div class="w-8/12 flex items-center justify-center overflow-hidden">
+            <img src="video.png" alt="" />
         </div>
     </div>
 
     <div class="container mx-auto py-5 ">
         <div class="title-border py-5">More videos</div>
-        <div class="flex justify-between">
+        <div
+            class="flex justify-between flex-wrap flex-column lg:flex-row
+            content-around">
             <VideoMini />
             <VideoMini />
             <VideoMini />
