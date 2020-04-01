be3bcd16ac4603bbc96fab882b67e7d317f8e98d
karlosgliberal <karlosgliberal@gmail.com>
2020-04-01 18:26:42 +0200

    Pruebas con las distancias

--- src/routes/danceposeaitor/index.svelte
+++ src/routes/danceposeaitor/index.svelte
@@ -10,13 +10,11 @@
     <NavSlim />
 
     <div class="flex flex-col xl:flex-row pt-12 mt-2">
-        <div class="w-full xl:w-1/3 flex items-center justify-center p-5">
+        <div class="w-full xl:w-1/3 flex items-center justify-center p-4">
 
             <div class="w-full h-auto bg-white border rounded-lg p-4">
                 <div class="flex content-center flex-col">
-                    <p class="font-lato text-xl text-center py-3">
-                        Click on the names to change the shapes
-                    </p>
+                    <p class="font-lato text-xl text-center py-3" />
                     <div class="pt-4">
                         <img class="center" src="dancehuman.svg" alt="" />
                     </div>
