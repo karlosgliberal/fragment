1b14294a85d4ffb176fc1ab2aa0788df315c65a4
Aitor Resano <aitor.rl@gmail.com>
2020-04-02 14:02:49 +0200

    ajustes tamaños

--- src/routes/danceposeaitor/sketch.svelte
+++ src/routes/danceposeaitor/sketch.svelte
@@ -12,8 +12,8 @@
 
     export let target;
     export let p5;
-    export let width = 500;
-    export let height = 500;
+    export let width = 700;
+    export let height = 520;
     export let movida = 'movida';
 
     $: cosa = movida;
@@ -59,7 +59,7 @@
     }
 
     export function setup() {
-        canvas = p5.createCanvas(500, 500);
+        canvas = p5.createCanvas(700, 525);
         canvas.mousePressed(canvasMousePressed);
         video = p5.createVideo('elvis.mp4', videoLoaded);
         poseNet = ml5.poseNet(video, modelReady);
@@ -128,7 +128,7 @@
     }
 
     function videoLoaded() {
-        video.size(500, 500);
+        video.size(700, 525);
     }
 
     function canvasMousePressed() {
