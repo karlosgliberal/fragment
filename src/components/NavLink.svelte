d36a56e15005173cb30e9d7d72782f30da6a0ae1
karlosgliberal <karlosgliberal@gmail.com>
2020-04-02 10:07:58 +0200

    Revert "Borrando sobras"
    This reverts commit 45d8eb4751067269793a8232317c71ca3952c253.

---
+++ src/components/NavLink.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { beforeUpdate } from 'svelte';
+    export let href;
+    export let active = false;
+
+    let className;
+
+    export { className as class };
+
+    beforeUpdate(() => {
+        console.log('the component is about to update');
+    });
+</script>
+
+<style type="text/postcss">
+    a {
+        @apply relative py-2 overflow-x-hidden block tracking-widest;
+
+        &::after {
+            @apply absolute w-full h-2 inset-x-0 bottom-0 transition duration-300 transform ease-in-out -translate-x-full;
+
+            content: '';
+        }
+
+        &:hover {
+            &::after {
+                @apply bg-blue-400;
+            }
+        }
+
+        &.active-link {
+            &::after {
+                @apply bg-red-500;
+            }
+        }
+
+        &:hover,
+        &.active-link {
+            &::after {
+                @apply translate-x-0;
+            }
+        }
+    }
+</style>
+
+<a {href} class:active-link={active} class={className} on:click>
+    <slot />
+</a>
