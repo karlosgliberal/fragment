ce8e499c993e76001fc021fb7f18f835f6b34f98
Aitor Resano <aitor.rl@gmail.com>
2020-04-02 17:49:03 +0200

    Limpieza

--- src/routes/danceposeaitor/index.svelte
+++ src/routes/danceposeaitor/index.svelte
@@ -19,23 +19,11 @@
     <NavSlim />
 
     <div class="flex flex-col xl:flex-row pt-12 mt-2">
-        <div class="flex justify-center">
 
-            <div class="bg-white xl:px-5 px-2 mx-2 xl:mx-10">
-                <div class="flex content-center flex-col">
-
-                    <div class="pt-4">
-                        <!-- <Dancehuman on:message={handleMessage} /> -->
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
         <div class="flex items-center column justify-center">
             <P5Canvas sketch={SimpleSketch} width="700" height="525" />
 
-            <div class="">
+            <!-- <div class="">
                 <div class="title-border py-3 text-center">Vídeos</div>
                 <div class="flex justify-center flex-col">
                     <VideoMini />
@@ -43,7 +31,7 @@
                     <VideoMini />
                     <VideoMini />
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 
