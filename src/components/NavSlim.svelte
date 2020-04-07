0553d61db366ad58d9f7d20ebf38697dca58d3cf
karlosgliberal <karlosgliberal@gmail.com>
2020-04-07 13:03:11 +0200

    Sistema del menu para el about

--- src/components/NavSlim.svelte
+++ src/components/NavSlim.svelte
@@ -2,6 +2,11 @@
     export let url = '';
     export let segment;
     console.log(segment);
+
+    function handleSubmit() {
+        console.log('mdfasd');
+        window.location.replace(`/dancepose`);
+    }
 </script>
 
 <div class="flex bg-black py-3">
@@ -13,8 +18,19 @@
     </div>
     <div class="w-1/3 flex items-center justify-end h-6">
         <div class="pr-4">
-            <a class="uppercase font-bold text-white" href={url}>About</a>
-
+            {#if segment === undefined}
+                <a
+                    href="dancepose/about"
+                    class="uppercase font-bold text-white">
+                    About
+                </a>
+            {:else}
+                <button
+                    class="uppercase font-bold text-white"
+                    on:click={handleSubmit}>
+                    Home
+                </button>
+            {/if}
         </div>
     </div>
 </div>
