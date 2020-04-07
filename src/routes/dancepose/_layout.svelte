d56373c308b214c685ccd82f8ba4b041af5e0f7c
Aitor Resano <aitor.rl@gmail.com>
2020-04-07 17:48:34 +0200

    Limipieza en css

--- src/routes/dancepose/_layout.svelte
+++ src/routes/dancepose/_layout.svelte
@@ -5,6 +5,69 @@
     export let segment;
 </script>
 
+<style>
+    input[type='checkbox'] {
+        background-color: transparent;
+    }
+
+    .dg .c input[type='checkbox'] {
+        margin-top: 12px !important;
+    }
+
+    select {
+        color: #fffcfc;
+        width: 100%;
+        height: 25px;
+        background-color: #303030;
+    }
+
+    .dg li:not(.folder) {
+        cursor: auto;
+        height: 40px !important;
+        line-height: 27px;
+        padding: 0 4px;
+    }
+
+    .dg .c .slider-fg {
+        background: white;
+        max-width: 100%;
+        height: 15px;
+        position: relative;
+        top: -6px;
+    }
+
+    .dg .slider {
+        float: left;
+        width: 66%;
+        margin-left: 0;
+        margin-right: 0;
+        height: 4px !important;
+        margin-top: 6px;
+        position: relative;
+        top: 13px;
+    }
+
+    .dg .cr.number input[type='text'] {
+        color: #ffffff !important;
+    }
+
+    .property-name {
+        padding-top: 3px !important;
+    }
+
+    .dg .c input[type='text'] {
+        border: 0;
+        margin-top: 4px;
+        padding: 3px;
+        width: 50%;
+        float: right;
+    }
+
+    .dg .slider {
+        margin-left: 0 !important;
+    }
+</style>
+
 <svelte:head>
     <title>Dance Pose</title>
     <meta name="segment" content={segment} />
