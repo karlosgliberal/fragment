80b8263f0488c60e864db35fafcbe5a5c230e7c3
karlosgliberal <karlosgliberal@gmail.com>
2020-04-14 12:52:57 +0200

    Intentando poner los scrpts al principio pero no puede ser, temas de sapper

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -10,7 +10,16 @@
     <meta name="keywords" content="" />
     <meta name="author" content="" />
     <meta name="segment" content={segment} />
+    <script src="https://cdn.jsdelivr.net/npm/p5@1.0.0/lib/p5.js">
 
+    </script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.6/dat.gui.min.js">
+
+    </script>
+    <script src="https://unpkg.com/ml5@0.5.0/dist/ml5.min.js">
+
+    </script>
 </svelte:head>
 
 <slot />
